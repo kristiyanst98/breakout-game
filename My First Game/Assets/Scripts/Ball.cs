daa91dd23c0dfe705a260f6b5ccc6bc76eb0d264
Kristiyan <kristiyanst98@gmail.com>
2020-11-29 14:40:29 +0200

    Add project

---
+++ My First Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float _speed = 20f;
+    Rigidbody _rigidbody;
+    Vector3 _velocity;
+    Renderer _renderer;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+        Invoke("Launch", 0.5f);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
+        _velocity = _rigidbody.velocity;
+        if (!_renderer.isVisible)
+        {
+            GameManager.Instance.Balls--; 
+            Destroy(gameObject);
+        }
+    }
+
+    void Launch()
+    {
+        _rigidbody.velocity = _speed * Vector3.up;
+    }
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _rigidbody.velocity = Vector3.Reflect(_velocity, collision.contacts[0].normal);
+    }
+}
+
---
+++ My First Game/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    float _speed = 20f;
+    void Start()
+    {    
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        var move = new Vector3(Input.GetAxisRaw("Horizontal"), _rigidbody.velocity.y, _rigidbody.velocity.z);
+        transform.position += move * _speed * Time.deltaTime;
+        //_rigidbody.MovePosition(new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 50)).x, -17, 0));  
+    }
+}
+
